cc7d06f765cda9aea2208e33e56ba90489ac5182
philipzamba54-ship-it <philipzamba54@gmail.com>
2026-04-15 09:31:39 +0300

    LAST

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -17,3 +17,8 @@ module.exports = (req, res, next) => {
         res.status(401).json({ message: 'Invalid token.' });
     }
 };
+const authheader=
+req.header('authorization');
+const token =authHeader && 
+authHeader.startWith('bearer')? authheader.split(''):
+null;
